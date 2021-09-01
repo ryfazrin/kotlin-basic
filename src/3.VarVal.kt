f3ddefdc4364bf05be5b39c9be5daf8879e74859
ryfazrin <riansera515@gmail.com>
2021-09-01 11:02:51 +0800

    add basic var and val

---
+++ src/3.VarVal.kt
@@ -0,0 +1,17 @@
+fun main() {
+    /**
+     * Basic Data Types & Variable
+     *
+     * var indentifier: Type = initialization
+     * var = bisa mengubah nilai
+     * val = tidak bisa mengubah nilai
+     * */
+    var company: String = "Dicoding"
+    println(company)
+
+    company = "Ryris"
+    println(company)
+
+    val old: Int = 19
+    println(old)
+}
