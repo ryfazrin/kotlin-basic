50bd08aaa3c109ae90f78f4cc3b77129896a3072
ryfazrin <riansera515@gmail.com>
2021-09-01 12:23:51 +0800

    update kotlin-basic to Aplikasi Penjualan Pulsa (basic)

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,41 @@
 fun main() {
-    println("Hello world")
+    // Static name
+    val agency: String = "Aplikasi Penjualan Pulsa"
+
+    /*
+     Header
+     */
+    println("================================")
+    println("=== $agency ===")
+    println("================================")
+
+    /*
+     * Body
+     */
+    println()
+    // Status
+    var status: Boolean = true
+    if (status) println("Aplikasi Aktif/Buka") else println("Aplikasi Tutup")
+
+    // Body Header
+    val bodyHeader = arrayOf("Daftar", "Pulsa")
+    print("${bodyHeader[0]} ${bodyHeader[1]} : ")
+
+    // List code
+    val rangeList = 0.rangeTo(4)
+    rangeList.forEach {
+        print("$it ")
+    }
+    println()
+
+    // daftar pulsa dan harga pulsa
+    val pulsaList = intArrayOf(5000, 10_000, 20_000, 50_0000, 100_000)
+    var hargaList = intArrayOf(7000, 12_000, 22_000, 52_0000, 102_000)
+
+    var lala: Array<IntArray> = arrayOf(pulsaList, hargaList)
+
+    println("Isi Pulsa : ")
+    for ((i, value) in pulsaList.withIndex()){
+        println("${i}. $value Harga : ${hargaList[i]}")
+    }
 }
