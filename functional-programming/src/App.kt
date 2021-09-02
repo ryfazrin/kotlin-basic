b3ed018384e16e3451433c6683119747f79cf47f
ryfazrin <riansera515@gmail.com>
2021-09-02 14:10:11 +0800

    update add avg nilai in functional programming

--- functional-programming/src/App.kt
+++ functional-programming/src/App.kt
@@ -12,24 +12,50 @@ fun main() {
 
     println(myFullName)
 
-    // * Nilai with vararg
-    val nilai = intArrayOf(10, 10, 10, 10, 10)
-    val mySumNilai = getSumNilai(nilai= nilai)
+    // ===========================
 
+    // * short about with NUll type & Elvis Operator (?:)
+    val about: String? = null
+    val myAbout = about ?: "Saya adalah seorang web/mobile developer dan bussiness man";
+
+    println(myAbout)
+
+    // ===========================
+
+    // * Nilai with array & vararg
+    val nilai = intArrayOf(10, 10, 10, 10, 10)
+
+    print("Nilai saya : ")
     nilai.forEachIndexed { index, value ->
         if (index == (nilai.size - 1))
             println("$value")
         else
             print("$value, ")
     }
+
+    // ===========================
+
+    val mySumNilai = getSumNilai(nilai = nilai)
+
+    print("Total : ")
     println(mySumNilai)
 
-    // * short about with NUll type & Elvis Operator (?:)
-    val about: String? = null
-    val myAbout = about ?: "Saya adalah seorang mobile developer dan bussiness man";
+    // ===========================
 
-    println(myAbout)
+    // * make function avg (rata-rata) with Function Type
+    val avg: Arithmetic = { valueA, valueB -> valueA / valueB }
+
+    // Avg Nilai
+    val avgNilai = avg(mySumNilai, nilai.size)
+    println("Rata-rata nilai : $avgNilai")
+
+    // ===========================
+
+
 }
+
+// * Arithmetic Function Type
+typealias Arithmetic = (Int, Int) -> Int
 
 /**
  * function for Get Full Name
